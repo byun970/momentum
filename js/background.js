efc26be2e107cfce7b0e37498624d83a19028712
byun970 <76036655+byun970@users.noreply.github.com>
2023-09-17 15:05:33 +0900

    수정04

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@ const images = ["0.jpg", "1.jpg", "2.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const url = `../img/${chosenImage}`;
+const url = `./img/${chosenImage}`;
 
 console.log(url);
 
